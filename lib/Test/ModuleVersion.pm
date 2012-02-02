a25b183ed31d877f73343f8ab8dc9545458e2889
Yuki Kimoto <kimoto.yuki@gmail.com>
2012-02-02 22:47:57 +0900

    fixed code

--- lib/Test/ModuleVersion.pm
+++ lib/Test/ModuleVersion.pm
@@ -1,6 +1,8 @@
 package Test::ModuleVersion;
 use Object::Simple -base;
 use ExtUtils::Installed;
+use HTTP::Tiny;
+use JSON;
 
 our $VERSION = '0.01';
 
@@ -12,9 +14,7 @@ has exclude => sub { [] };
 has show_lack_module_url => 0;
 
 sub _lack_module_url {
-  require HTTP::Tiny;
-  require JSON;
-  require Test::ModuleVersion;
+  my $failed = shift;
 
   my $outputs = [];
 
@@ -30,13 +30,13 @@ sub _lack_module_url {
     # Module info
     my $module_dist = $module;
     $module_dist =~ s/::/-/g;
-    my $version = $failed{$module}->{version};
+    my $version = $failed->{$module}{version};
     
     # metaCPAN api
-    my $res = $http->get("$metacpan_api/$moduel_dist");
+    my $res = $http->get("$metacpan_api/$module_dist");
     push @$outputs, "# $module url is unknown" and next
       unless $res->{success};
-    $content = $res->{content};
+    my $content = $res->{content};
 
     # Latest release
     my $latest_release = JSON::json_decode $content;
@@ -45,11 +45,12 @@ sub _lack_module_url {
     my $index2 = substr $author, 1, 1;
     
     # metaCPAN archive
-    $res = $http->head("$metacpan_archive/$module_dist-$version.tar.gz");
+    my $module_url = "$metacpan_archive/$module_dist-$version.tar.gz";
+    $res = $http->head($module_url);
     if ($res->{success}) { push @$outputs, "# $module_url" }
     else {
       # Backpan archive
-      my $backpan = "http://backpan.perl.org/authors/id"
+      my $backpan = "http://backpan.perl.org/authors/id";
       my $module_url = "$backpan/$index1/$index2/"
         . "$author/$module_dist-${version}.tar.gz";
       my $res = $http->head($module_url);
@@ -82,7 +83,7 @@ sub module_version_is {
   is($got, $expected, "$module version: $expected");
 }
 
-my %failed;
+my $failed = {};
 my $use_ok;
 my $version_ok;
 
@@ -93,15 +94,13 @@ EOS
     my $version = $ei->version($module);
     $code .= "\$use_ok = use_ok('$module');\n"
       . "\$version_ok = module_version_is('$module', \$ei->version('$module'), '$version');\n"
-      . "\$failed{$module} = {version => $version} unless \$use_ok && \$version_ok;\n\n";
+      . "\$failed->{$module} = {version => $version} unless \$use_ok && \$version_ok;\n\n";
   }
   
   if ($self->show_lack_module_url) {
-
     $code .= <<'EOS';
-my $outputs = Test::ModuleVersion::_lack_module_url;
+my $outputs = Test::ModuleVersion::_lack_module_url($failed);
 print "\n", @$outputs;
-
 EOS
   }
   
